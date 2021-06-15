6910b1980ef4254474924eb53d1b37d10284ea3b
skylarsoehl <skysoehl@gmail.com>
2021-06-15 13:19:33 -0400

    created outline for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,19 @@
+/* eslint-disable vue/no-multiple-template-root */
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">portfolio-2021</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="nav">nav</div>
+    <div class="intro">
+      <p>
+        Welcome to my little corner of the web. I am a curious person driven by
+        compassion, social justice, and adventure.
+      </p>
     </div>
+    <div class="instruct">
+      <div>arrow here</div>
+      <div>Scroll to view my work</div>
+    </div>
+    <div class="project list">project list here</div>
+    <div class="contact">contact form here</div>
   </div>
 </template>
 
@@ -29,35 +21,4 @@
 export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
